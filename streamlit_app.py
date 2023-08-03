6b8e2ebfa3250b868a7e70359edbc2dd5e3703d2
navneetcanada <141301187+navneetcanada@users.noreply.github.com>
2023-08-03 02:07:43 -0400

    Added breakfast menu options

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and Blueberry oatmeal')
+streamlit.text('Kale, Spinach and Rocket smoothie')
+streamlit.text('Hard-Boiled Free-Range eggs')
